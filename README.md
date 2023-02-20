5a0c9cedcd8c430ca0d58f0b34e93922c09a816e
Onadeko Oluwateniola <onadekoteniolalillian@gmail.com>
2023-02-20 16:56:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<!-- Question 1 -->
+
+Arithmetic operators-> {+(addition), -(subtraction), *(multiplication), /(division), %(modulus)}
+Assignment operators-> {=}
+Comparison operators-> { >, <, >=, <=}
+Logical operators {&&, ||, !}
+Bitwise operators {}
+
+
+<!-- Question 2 -->
+
+Arithmetic operators: 2+5 , 10/2
+Assignment operators : let firstName = "Teniola"
+Comparison operators: 35 <= 50, 20 >= 20
+Logical operators : 5 > 4 &&  2<= 5
+
+
+<!-- Question 4 -->
+The results are : 1, 8, 15.
---
+++ index.js
@@ -0,0 +1,36 @@
+// Question 3
+
+
+// declaration of subjects
+
+const scienceSubjects = "Physics, Chemistry, Biology, Technical Writing"
+const socialScienceSubjects = "Accounting, Commerce, Marketing, Geography"
+const artsSubjects = "Government, Literature, Economics, History"
+const generalSubjects = "English, Mathematics"
+
+let bolatitoSubjects = artsSubjects
+
+// if-else statement
+
+if (bolatitoSubjects === scienceSubjects){
+    console.log(`Bolatito's subject combinations are ${scienceSubjects}`)
+
+} else if (bolatitoSubjects === socialScienceSubjects){
+
+    console.log(`Bolatito's subject combinations are ${socialScienceSubjects}`)
+
+}else if (bolatitoSubjects === artsSubjects){
+    
+    console.log(`Bolatito's subject combinations are ${artsSubjects}`)
+
+}else {
+    console.log(`Bolatito's subject combinations are ${generalSubjects}`)
+}
+   
+
+
+// Question 5
+const num = 40
+let pwr = 40/2**2
+
+
